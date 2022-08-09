a3a890959463eaa259d85e1e81cdcc348ec7edc2
Jose Alves <jose.sousalves@gmail.com>
2022-08-09 11:47:48 +0100

    feat(ui): adds ui styles files

---
+++ packages/ui/styles/theme/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    gray50: '#FAFAFA',
+    gray100: '#F5F5F5',
+    gray200: '#E5E5E5',
+    gray300: '#D4D4D4',
+    gray400: '#A3A3A3',
+    gray500: '#737373',
+    gray600: '#525252',
+    gray700: '#404040',
+    gray800: '#262626',
+    gray900: '#171717',
+    primary: '#E50914',
+  },
+
+  fonts: {
+    roboto: '"Roboto", sans-serif',
+  },
+
+  dropShadow: '0px 1px 6px rgba(0, 0, 0, 0.1)',
+};
+
+export default theme;
