94051b5206a3900b1b8b674024231f34f1fe41e3
Jose Alves <jose.sousalves@gmail.com>
2022-08-09 15:53:50 +0100

    feat(prettier): prettier changes

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+
 import { Card } from '@ui';
 
 const Home: NextPage = () => {
--- packages/ui/components/Card/Card.tsx
+++ packages/ui/components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import * as S from './Card.styles';
 import { CardProps } from './Card.types';
 
-const Card = ({ children, ...rest }: CardProps) => {
+const Card: React.FC = ({ children, ...rest }: CardProps) => {
   return <S.Card {...rest}>{children}</S.Card>;
 };
 
