8a93dae3a57be97effdb700ab47eb47be44c7327
Jose Alves <jose.sousalves@gmail.com>
2022-08-09 12:10:08 +0100

    feat(frontend): setup and config frontend next app

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { FC, useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import SEO from '@config/next-seo';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'ui/styles';
+import theme from 'ui/styles/theme';
+import { DefaultSeo } from 'next-seo';
+
+const MyApp: FC<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  const [mounted, setMounted] = useState(false);
+  const { asPath } = useRouter();
+
+  const page = asPath === '/' ? '' : asPath;
+  const [canonicalUrl] = `https://radflix.com${page}`.split('?');
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <DefaultSeo
+          canonical={canonicalUrl}
+          openGraph={{
+            url: canonicalUrl,
+            type: 'website',
+            site_name: 'Radflix',
+          }}
+          dangerouslySetAllPagesToNoIndex
+          {...SEO}
+        />
+
+        <GlobalStyle />
+        {mounted && <Component {...pageProps} />}
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default MyApp;
---
+++ apps/frontend/src/config/next-seo.ts
@@ -0,0 +1,9 @@
+export default {
+  titleTemplate: '%s | MERN Test',
+  defaultTitle: 'MERN Project',
+  twitter: {
+    handle: '@mern_project',
+    site: '@mern_project',
+    cardType: 'summary_large_image',
+  },
+};
