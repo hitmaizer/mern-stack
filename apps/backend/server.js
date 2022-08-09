01b5c919047028f7ca78290bea5286bb939ede35
Jose Alves <jose.sousalves@gmail.com>
2022-08-09 16:08:57 +0100

    feat(backend): sends response to / on port 4000

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 //express app
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to the app' });
+});
+
 //listen for requests
 app.listen(4000, (req, res) => {
   console.log('listening on port 4000');
