7b9fb57e1a7622763d0ff9076bc3a1b464d0655c
Jose Alves <jose.sousalves@gmail.com>
2022-08-11 14:28:32 +0100

    feat(Grid): adds Grid component files

---
+++ packages/ui/components/Grid/Grid.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 100px;
+  margin: 0 auto;
+  padding: 32px;
+`;
---
+++ packages/ui/components/Grid/Grid.tsx
@@ -0,0 +1,8 @@
+import * as S from './Grid.styles';
+import { GridProps } from './Grid.types';
+
+const Grid = ({ children, ...rest }: GridProps) => {
+  return <S.Grid {...rest}>{children}</S.Grid>;
+};
+
+export default Grid;
