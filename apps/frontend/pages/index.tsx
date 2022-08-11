9fc05b977a7d2f9f6d7db1c62393d1c7bdd0fa50
Jose Alves <jose.sousalves@gmail.com>
2022-08-11 11:32:25 +0100

    feat(frontend): fetches Workouts data/ saves on local state/ maps

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,9 +1,27 @@
-import type { NextPage } from 'next';
+/* eslint-disable no-underscore-dangle */
+import { useEffect, useState } from 'react';
+
+import type { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 
-import { Navbar } from '@ui';
+import { Navbar, Stack, Workouts } from '@ui';
 
-const Home: NextPage = () => {
+export interface Workout {
+  title: string;
+  reps: number;
+  load: number;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const Home: NextPage = ({ allWorkouts }: any) => {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    setWorkouts(allWorkouts);
+  }, []);
+  console.log(workouts);
   return (
     <div>
       <Navbar>
@@ -11,8 +29,36 @@ const Home: NextPage = () => {
           <h1>Workout Buddy</h1>
         </Link>
       </Navbar>
+      {workouts && (
+        <>
+          <Stack display="flex">
+            {workouts.map((workout: Workout) => {
+              return (
+                <Workouts
+                  key={workout._id}
+                  title={workout.title}
+                  load={workout.load}
+                  reps={workout.reps}
+                  created={workout.createdAt}
+                />
+              );
+            })}
+          </Stack>
+        </>
+      )}
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const req = await fetch('http://localhost:4000/api/workouts');
+  const allWorkouts = await req.json();
+
+  return {
+    props: {
+      allWorkouts,
+    },
+  };
+};
