b40a99ba0c680261ac76f71112058724eae58119
Jose Alves <jose.sousalves@gmail.com>
2022-08-11 11:30:10 +0100

    feat(Box): adds Box components files

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,2 +1,5 @@
+export { default as Box } from './Box';
 export { default as Card } from './Card';
 export { default as Navbar } from './Navbar';
+export { default as Stack } from './Stack';
+export { default as Workouts } from './Workouts';
