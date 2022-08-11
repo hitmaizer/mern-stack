a50ece7e49909051d43ec2cfdb5ccdef2bb8e887
Jose Alves <jose.sousalves@gmail.com>
2022-08-11 14:29:07 +0100

    style(Workout): updates Workout component basic styling

--- packages/ui/components/Workouts/Workouts.styles.ts
+++ packages/ui/components/Workouts/Workouts.styles.ts
@@ -9,9 +9,29 @@ export const Workouts = styled.div`
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
   display: flex;
   flex-direction: column;
-  gap: 16px;
+  gap: 8px;
+  min-width: 1400px;
+
+  p {
+    margin: 0;
+    font-size: 0.9em;
+    color: #555;
+  }
+
+  span {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    cursor: pointer;
+    background: #f1f1f1;
+    padding: 6px;
+    border-radius: 50%;
+    color: #333;
+  }
 `;
 
 export const Title = styled.h1`
   font-size: 1.5rem;
+  margin: 0 0 10px 0;
+  color: ${({ theme }) => theme.colors.primary};
 `;
