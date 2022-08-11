8ecfd9b7bcd0e1cdb2eee08995dcf40b1c098bd6
Jose Alves <jose.sousalves@gmail.com>
2022-08-11 15:08:31 +0100

    feat(WorkoutForm): adds WorkoutForm component markup and local state

--- packages/ui/components/WorkoutForm/WorkoutForm.tsx
+++ packages/ui/components/WorkoutForm/WorkoutForm.tsx
@@ -4,13 +4,72 @@ import * as S from './WorkoutForm.styles';
 import { WorkoutFormProps } from './WorkoutForm.types';
 
 const WorkoutForm = ({ children, ...rest }: WorkoutFormProps) => {
-  const [title, setTitle] = useState('');
-  const [load, setLoad] = useState(0);
-  const [reps, setReps] = useState(0);
+  const [title, setTitle] = useState<string>('');
+  const [load, setLoad] = useState<string | number>(0);
+  const [reps, setReps] = useState<string | number>(0);
+  const [error, setError] = useState<string | null>('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const workout = { title, load, reps };
+
+    const res = await fetch('http://localhost:4000/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type:': 'application/json',
+      },
+    });
+
+    const json = await res.json();
+
+    if (!res.ok) {
+      setError(json.error);
+    }
+    if (res.ok) {
+      setTitle('');
+      setLoad('');
+      setReps('');
+      setError(null);
+      console.log('new workout created');
+    }
+  };
 
   return (
     <>
-      <S.WorkoutForm {...rest}>{children}</S.WorkoutForm>
+      <S.WorkoutForm {...rest}>
+        <form action="" onSubmit={handleSubmit}>
+          <h3>Add a new Workout</h3>
+          <label htmlFor="title">
+            Excercise Title:
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+          </label>
+          <label htmlFor="load">
+            Load (KG):
+            <input
+              type="number"
+              onChange={(e) => setLoad(e.target.value)}
+              value={load}
+            />
+          </label>
+          <label htmlFor="reps">
+            Reps :
+            <input
+              type="number"
+              onChange={(e) => setReps(e.target.value)}
+              value={reps}
+            />
+          </label>
+          <button type="submit">Add Workout</button>
+        </form>
+        {error && <p>{error}</p>}
+        {children}
+      </S.WorkoutForm>
     </>
   );
 };
