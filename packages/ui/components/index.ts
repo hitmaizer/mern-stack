88b98f94f2c399f009cb1f6a2ebd108a9321383f
Jose Alves <jose.sousalves@gmail.com>
2022-08-12 15:50:00 +0100

    feat(ui): updates ui components index exports file

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -2,6 +2,5 @@ export { default as Box } from './Box';
 export { default as Card } from './Card';
 export { default as Grid } from './Grid';
 export { default as Logo } from './Logo';
-export { default as Navbar } from './Navbar';
 export { default as Stack } from './Stack';
 export { default as Workouts } from './Workouts';
