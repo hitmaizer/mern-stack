6104ba3472d9aad3b53f8fbcea6f4529169e0c46
Jose Alves <jose.sousalves@gmail.com>
2022-08-12 10:27:19 +0100

    feat(Workouts): updates Workout prop types file

--- packages/ui/components/Workouts/Workouts.types.ts
+++ packages/ui/components/Workouts/Workouts.types.ts
@@ -1,9 +1,19 @@
 import { ReactNode } from 'react';
 
+export interface Workout {
+  title: string;
+  reps: number;
+  load: number;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
 export interface WorkoutsProps {
   children?: ReactNode;
   title?: string;
   load?: number;
   reps?: number;
   created?: string;
+  handleClick: (a: Workout) => void;
+  data?: Workout;
 }
