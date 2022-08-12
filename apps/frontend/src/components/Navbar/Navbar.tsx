7fa7b6de60540b4fe718fe50ac124b598379e429
Jose Alves <jose.sousalves@gmail.com>
2022-08-12 15:56:57 +0100

    feat(Navbar): adds navigation to Navbar component

--- apps/frontend/src/components/Navbar/Navbar.tsx
+++ apps/frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-import { Logo } from '@ui';
+import { Logo, Stack } from '@ui';
 
 import * as S from './Navbar.styles';
 import { NavbarProps } from './Navbar.types';
@@ -12,6 +12,12 @@ const Navbar = ({ children, ...rest }: NavbarProps) => {
         <Link href="/">
           <Logo>Workout Buddy</Logo>
         </Link>
+        <nav>
+          <Stack display="flex" gridGap="8px">
+            <Link href="/login">Login</Link>
+            <Link href="/signup">Signup</Link>
+          </Stack>
+        </nav>
         {children}
       </S.Container>
     </S.Navbar>
