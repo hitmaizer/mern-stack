151bb46eaf9a8cf3deda983e96fa7e05cf679364
Jose Alves <jose.sousalves@gmail.com>
2022-08-12 15:49:41 +0100

    feat(components): updates components index exports file

--- apps/frontend/src/components/index.ts
+++ apps/frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as LoginForm } from './LoginForm';
+export { default as Navbar } from './Navbar';
 export { default as SignupForm } from './SignupForm';
 export { default as WorkoutForm } from './WorkoutForm';
