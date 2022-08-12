39f31d6dceba081158d22cd37969a4000c18d043
Jose Alves <jose.sousalves@gmail.com>
2022-08-12 11:52:38 +0100

    refactor(userController): updates userController signup function

--- apps/backend/controllers/userController.js
+++ apps/backend/controllers/userController.js
@@ -7,7 +7,15 @@ const login_user = async (req, res) => {
 
 // signup user
 const signup_user = async (req, res) => {
-  res.json({ msg: 'Signup user' });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
