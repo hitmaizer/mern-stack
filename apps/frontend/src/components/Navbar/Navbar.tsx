57f8cd701e873e0fa162c542ff05bcb1bc9325a8
Jose Alves <jose.sousalves@gmail.com>
2022-08-12 17:07:00 +0100

    refactor(Navbar): conditionally renders btns based on user session

--- apps/frontend/src/components/Navbar/Navbar.tsx
+++ apps/frontend/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useAuthContext } from 'src/hooks/useAuthContext';
 import { useLogout } from 'src/hooks/useLogout';
 
 import { Logo, Stack } from '@ui';
@@ -8,6 +9,7 @@ import { NavbarProps } from './Navbar.types';
 
 const Navbar = ({ children, ...rest }: NavbarProps) => {
   const { logout } = useLogout();
+  const { user }: any = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -20,15 +22,20 @@ const Navbar = ({ children, ...rest }: NavbarProps) => {
           <Logo>Workout Buddy</Logo>
         </Link>
         <nav>
-          <div>
-            <S.LogoutBtn type="button" onClick={handleClick}>
-              Log out
-            </S.LogoutBtn>
-          </div>
-          <Stack display="flex" gridGap="8px">
-            <Link href="/login">Login</Link>
-            <Link href="/signup">Signup</Link>
-          </Stack>
+          {user && (
+            <Stack display="flex" gridGap="8px" alignItems="center">
+              <span>{user.email}</span>
+              <S.LogoutBtn type="button" onClick={handleClick}>
+                Log out
+              </S.LogoutBtn>
+            </Stack>
+          )}
+          {!user && (
+            <Stack display="flex" gridGap="8px">
+              <Link href="/login">Login</Link>
+              <Link href="/signup">Signup</Link>
+            </Stack>
+          )}
         </nav>
         {children}
       </S.Container>
